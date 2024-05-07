aad6d3f9e040fa189268bd8767ed97f7daa8c41b
Sofiia Folvarochna <sofiyafolv@gmail.com>
2024-05-07 16:51:56 +0300

    add homework 3

--- facade-service/FacadeController.py
+++ facade-service/FacadeController.py
@@ -1,29 +1,30 @@
 from flask import jsonify, request, Flask, make_response
 import requests
 import uuid
-
+import random
 
 class FacadeController:
     def __init__(self):
         self.facade_service = Flask(__name__)
+        self.logging_ports = [8082, 8083, 8084]
 
         @self.facade_service.route('/facade_service', methods=['POST'])
         def post_request():
             data = request.get_json()
             print(data)
             unique_uuid = str(uuid.uuid4())
-            requests.post('http://localhost:8081/logging_service', json={'uuid': unique_uuid, 'msg': data})
+            requests.post(f'http://localhost:{random.choice(self.logging_ports)}/logging_service', json={'uuid': unique_uuid, 'msg': data})
             return '', 200
 
         @self.facade_service.route('/facade_service', methods=['GET'])
         def get_response():
-            logging_service_response = requests.get('http://localhost:8081/logging_service')
-            message_service_response = requests.get('http://localhost:8082/messages_service')
+            logging_service_response = requests.get(f'http://localhost:{random.choice(self.logging_ports)}/logging_service')
+            message_service_response = requests.get('http://localhost:8085/messages_service')
 
             return jsonify(logging_service_response.json(), message_service_response.json())
 
     def run(self):
-        self.facade_service.run(port=8080, debug=True)
+        self.facade_service.run(port=8081, debug=True)
 
 
 if __name__ == '__main__':
--- logging-service/LoggingController.py
+++ logging-service/LoggingController.py
@@ -1,10 +1,12 @@
 from flask import jsonify, request, Flask, make_response
-
+import argparse
+import hazelcast
 
 class LoggingController:
     def __init__(self):
         self.logging_service = Flask(__name__)
-        self.messages = {}
+        self.client = hazelcast.HazelcastClient()
+        self.messages_map = self.client.get_map("logging_service_messages").blocking()
 
         @self.logging_service.route('/logging_service', methods=['POST'])
         def post_request():
@@ -12,18 +14,25 @@ class LoggingController:
             print("Received:", data)
             unique_uuid = data['uuid']
             msg = data['msg']
-            self.messages[unique_uuid] = msg
+            self.messages_map.put(unique_uuid, msg)
             print("Message received: " + msg + ". Saved with unique id: " + unique_uuid)
             return '', 200
 
         @self.logging_service.route('/logging_service', methods=['GET'])
         def get_response():
-            return jsonify(list(self.messages.values()))
+            return jsonify(list(self.messages_map.values()))
 
-    def run(self):
-        self.logging_service.run(port=8081, debug=True)
+    def run(self, logging_port):
+        try:
+            self.logging_service.run(port=logging_port, debug=True)
+        finally:
+            self.client.shutdown()
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('logging_port')  
+    args = parser.parse_args()
+
     app = LoggingController()
-    app.run()
+    app.run(args.logging_port)

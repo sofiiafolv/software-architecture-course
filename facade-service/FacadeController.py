303ce04bfe0f0727bb8f4dcd77617419b73093ab
Sofiia Folvarochna <sofiyafolv@gmail.com>
2024-05-07 17:54:01 +0300

    minor

--- facade-service/FacadeController.py
+++ facade-service/FacadeController.py
@@ -2,24 +2,29 @@ from flask import jsonify, request, Flask, make_response
 import requests
 import uuid
 import random
+import hazelcast
 
 class FacadeController:
     def __init__(self):
         self.facade_service = Flask(__name__)
         self.logging_ports = [8082, 8083, 8084]
+        self.messaging_ports = [8085, 8086]
+        self.client = hazelcast.HazelcastClient()
+        self.messaging_queue = self.client.get_queue("messaging_queue")
 
         @self.facade_service.route('/facade_service', methods=['POST'])
         def post_request():
             data = request.get_json()
             print(data)
             unique_uuid = str(uuid.uuid4())
+            self.messaging_queue.put(data)
             requests.post(f'http://localhost:{random.choice(self.logging_ports)}/logging_service', json={'uuid': unique_uuid, 'msg': data})
             return '', 200
 
         @self.facade_service.route('/facade_service', methods=['GET'])
         def get_response():
             logging_service_response = requests.get(f'http://localhost:{random.choice(self.logging_ports)}/logging_service')
-            message_service_response = requests.get('http://localhost:8085/messages_service')
+            message_service_response = requests.get(f'http://localhost:{random.choice(self.messaging_ports)}/messages_service')
 
             return jsonify(logging_service_response.json(), message_service_response.json())
 
--- messages-service/MessagesController.py
+++ messages-service/MessagesController.py
@@ -1,5 +1,6 @@
 from flask import jsonify, Flask
-
+import argparse
+import hazelcast
 
 class MessagesController:
     def __init__(self):
@@ -9,10 +10,14 @@ class MessagesController:
         def get_message():
             return jsonify("not implemented yet")
 
-    def run(self):
-        self.messages_service.run(port=8085, debug=True)
+    def run(self, messaging_port):
+        self.messages_service.run(port=messaging_port, debug=True)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('messaging_port')  
+    args = parser.parse_args()
+
     messages_controller = MessagesController()
-    messages_controller.run()
+    messages_controller.run(args.messaging_port)
